13630e393431d7c0b17b300e4c37a7fb455a9d24
yiao <yiao.sun@supinfo.com>
2016-05-16 11:15:19 +0200

    All activity

---
+++ app/src/main/java/projetfinannee/supinfo/com/projetfinanneeand/activity_uesrprofie.java
@@ -0,0 +1,13 @@
+package projetfinannee.supinfo.com.projetfinanneeand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_uesrprofie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_uesrprofie);
+    }
+}
